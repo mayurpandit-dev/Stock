205ec6e08587e724102e0a66d64411a680961cba
mayurpandit-dev <79525120+mayurpandit-dev@users.noreply.github.com>
2021-02-23 20:20:29 +0530

    Added Stock Project Source Code

---
+++ Stock/Stock/Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NewMethod();
+        }
+
+        private void NewMethod()
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //TO:DO: Check Login username and password
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=SD0003;Integrated Security=True");
+            SqlDataAdapter sqa = new SqlDataAdapter(@"SELECT *
+                FROM [SD0003].[dbo].[Login] Where UserName = '"+textBox1.Text+"' and Password = '"+textBox2.Text+"'",con);
+            DataTable dt = new DataTable();
+            sqa.Fill(dt);
+            if(dt.Rows.Count == 1)
+            {
+                this.Hide();
+                StockMain main = new StockMain();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username & Password..!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                NewMethod();
+            }
+        }
+    }
+}
---
+++ Stock/Stock/Products.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stock
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Products_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            textBox1.Focus();
+
+            //Reading Data
+            LoadData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=SD0003;Integrated Security=True");
+            //Insert logic
+            con.Open();
+            bool status = false;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                status = true;
+            }else
+            {
+                status = false;
+            }
+           
+            var sqlQuery = "";
+            if (IfProductsExists(con,textBox1.Text))
+            {
+                sqlQuery = @"UPDATE [dbo].[Products] SET [ProductName] = '" + textBox2.Text + "',[ProductStatus] ='" + status + "' WHERE [ProductCode] = '" + textBox1.Text + "'";
+            }
+            else
+            {
+                sqlQuery = @"INSERT INTO [dbo].[Products] ([ProductCode],[ProductName],[ProductStatus]) VALUES 
+                ('" + textBox1.Text + "','" + textBox2.Text + "','" + status + "')";
+            }
+
+
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            //Reading Data
+             LoadData();
+
+        }
+        private bool IfProductsExists(SqlConnection con,string productCode)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("Select 1 From [dbo].[Products] where [ProductCode] = '" + productCode +"'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+        public void LoadData()
+        {
+            //Reading Data
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=SD0003;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("Select * From [dbo].[Products]  ", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = item["ProductCode"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item["ProductName"].ToString();
+                if ((bool)item["ProductStatus"])
+                {
+                    dataGridView1.Rows[n].Cells[2].Value = "Active";
+                }
+                else
+                {
+                    dataGridView1.Rows[n].Cells[2].Value = "Deactive";
+                }
+            }
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            if (dataGridView1.SelectedRows[0].Cells[2].Value.ToString() == "Active")
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            else
+            {
+                comboBox1.SelectedIndex = 1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=SD0003;Integrated Security=True");
+            var sqlQuery = "";
+            if (IfProductsExists(con, textBox1.Text))
+            {
+                con.Open();
+                sqlQuery = @"DELETE FROM [dbo].[Products] WHERE [ProductCode] = '" + textBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("Record Not Exits....");
+            }
+
+            //Reading Data
+            LoadData();
+        }
+    }
+}
